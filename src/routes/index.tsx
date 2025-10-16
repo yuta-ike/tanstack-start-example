04e58550e9784b5aa0e62e4bb2a1774a12309836
yuta-ikeoku <yuta-ikeoku@m3.com>
2025-10-16 22:31:25 +0900

    fix: use npm

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import { createServerFn } from "@tanstack/react-start";
 
 const getNum = createServerFn({
   method: "GET",
-}).handler<any>(() => 1); // remove <any> to return 1
+}).handler<number>(() => 1); // remove <number> to return 1
 
 export const Route = createFileRoute("/")({
   component: App,
